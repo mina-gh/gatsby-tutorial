41b209afd784956c06e7a40ad764a92721110af1
mina-gh <minamail@gmail.com>
2021-06-15 16:22:55 -0400

    Gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
+
+// Note the Link component is for linking two pages within the same Gatsby site.
+// Use the regular HTML <a> for external links
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+
+      <Link to="/about/">About</Link>
+
+      <Link to="/contact/">Contact</Link>
+    </div>
+  )
 }
